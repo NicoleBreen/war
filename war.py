ef66393544ab4967008e46f36316d046ea94ee92
Nicole <n.breen@hotmail.co.uk>
2024-10-21 16:08:56 +0100

    card_class

---
+++ war.py
@@ -0,0 +1,12 @@
+import random
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self):
+        return self.rank + " of " + self.suit
+    
+two_hearts = Card("Hearts","Two")
+print(two_hearts)

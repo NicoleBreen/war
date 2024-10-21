327550a6d7d1b5e348a8f4d773b474e21b9a1700
Nicole <n.breen@hotmail.co.uk>
2024-10-21 16:29:56 +0100

    component

---
+++ card.py
@@ -0,0 +1,23 @@
+import random
+# needs to be title case atm
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+# game logic
+two_hearts = Card("Hearts","Two")
+three_of_clubs = Card("Clubs","Three")
+
+# is the two of hearts the same value as three of clubs?
+result = two_hearts.value == three_of_clubs.value
+print(result) # False
